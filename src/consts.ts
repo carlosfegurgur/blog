23ac445fcb3c12f4229872e76457955fbfa63b9f
Carlos Fegurgur <carlosfegurgur@gmail.com>
2026-04-26 17:45:50 -0500

    - Ui overhaul - used claude to generate most boilerplate - initial landing page information/scaffolding

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Carlos Fegurgur';
+export const SITE_TITLE_SHORT = 'CF';
+export const SITE_DESCRIPTION = 'Personal Website of Carlos Fegurgur';
